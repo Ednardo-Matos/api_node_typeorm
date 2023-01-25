323457829efea05b72e967bb93306eab03b2f1b7
Ednardo-Matos <ednardoinacio@outlook.com>
2023-01-24 22:58:12 -0300

    iniciando projeto

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import * as express from "express"
+import * as bodyParser from "body-parser"
+import { AppDataSource } from "./data-source"
+import routes from "./routes"
+
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(routes)
+
+app.listen(3000)
